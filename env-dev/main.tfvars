25e703f2602ddb83d06f4578b407c853e203a6e5
sai <kuppalasaivikram@gmail.com>
2024-06-11 14:46:50 +0530

    common module to create resource and various project module are created

--- env-dev/main.tfvars
+++ env-dev/main.tfvars
@@ -1,1 +1,21 @@
-env=dev
+env = "dev"
+components = {
+  frontend = {
+    name          = "frontend"
+    instance_type = "t3.small"
+    port_no       = 80
+
+  }
+  mysql = {
+    name          = "mysql"
+    instance_type = "t3.small"
+    port_no       = 80
+
+  }
+  backend = {
+    name          = "backend"
+    instance_type = "t3.small"
+    port_no       = 8080
+  }
+
+}
--- main.tf
+++ main.tf
@@ -1,3 +1,28 @@
-module "app" {
+module "frontend" {
+  depends_on = [module.backend]
   source = "git::https://github.com/vikramdevopsb79/tf-module-app.git"
+  instance_type = var.components["frontend"]["instance_type"]
+  name          = var.components["frontend"]["name"]
+  env           = var.env
+  port_no       = var.components["frontend"]["port_no"]
+
+}
+
+module "backend" {
+  depends_on = [module.mysql]
+  source = "git::https://github.com/vikramdevopsb79/tf-module-app.git"
+  instance_type = var.components["backend"]["instance_type"]
+  name          = var.components["backend"]["name"]
+  env           = var.env
+  port_no       = var.components["backend"]["port_no"]
+
+}
+
+module "mysql" {
+  source = "git::https://github.com/vikramdevopsb79/tf-module-app.git"
+  instance_type = var.components["mysql"]["instance_type"]
+  name          = var.components["mysql"]["name"]
+  env           = var.env
+  port_no       = var.components["mysql"]["port_no"]
+
 }
