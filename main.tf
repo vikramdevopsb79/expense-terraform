6b104ad2757a54e74b7dad2df7b6f2abccbd8cff
sai <kuppalasaivikram@gmail.com>
2024-06-21 13:08:22 +0530

    APP Frontend

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "frontend" {
-  depends_on = [module.backend]
+#   depends_on = [module.backend]
   source = "git::https://github.com/vikramdevopsb79/tf-module-app.git"
   instance_type = var.components["Frontend"]["instance_type"]
   name          = var.components["Frontend"]["name"]
@@ -11,29 +11,29 @@ module "frontend" {
   zone_id       = data.aws_route53_zone.main.zone_id
 }
 
-module "backend" {
-  depends_on = [module.mysql]
-  source = "git::https://github.com/vikramdevopsb79/tf-module-app.git"
-  instance_type = var.components["backend"]["instance_type"]
-  name          = var.components["backend"]["name"]
-  env           = var.env
-  port_no       = var.components["backend"]["port_no"]
-  vault_token   = var.vault_token
-  SSH_PASSWORD = var.SSH_PASSWORD
-  prometheus_servers = var.prometheus_servers
-  zone_id       = data.aws_route53_zone.main.zone_id
-
-}
-
-module "mysql" {
-  source = "git::https://github.com/vikramdevopsb79/tf-module-app.git"
-  instance_type = var.components["mysql"]["instance_type"]
-  name          = var.components["mysql"]["name"]
-  env           = var.env
-  port_no       = var.components["mysql"]["port_no"]
-  vault_token   = var.vault_token
-  SSH_PASSWORD = var.SSH_PASSWORD
-  prometheus_servers = var.prometheus_servers
-  zone_id       = data.aws_route53_zone.main.zone_id
-
-}
+# module "backend" {
+#   depends_on = [module.mysql]
+#   source = "git::https://github.com/vikramdevopsb79/tf-module-app.git"
+#   instance_type = var.components["backend"]["instance_type"]
+#   name          = var.components["backend"]["name"]
+#   env           = var.env
+#   port_no       = var.components["backend"]["port_no"]
+#   vault_token   = var.vault_token
+#   SSH_PASSWORD = var.SSH_PASSWORD
+#   prometheus_servers = var.prometheus_servers
+#   zone_id       = data.aws_route53_zone.main.zone_id
+#
+# }
+#
+# module "mysql" {
+#   source = "git::https://github.com/vikramdevopsb79/tf-module-app.git"
+#   instance_type = var.components["mysql"]["instance_type"]
+#   name          = var.components["mysql"]["name"]
+#   env           = var.env
+#   port_no       = var.components["mysql"]["port_no"]
+#   vault_token   = var.vault_token
+#   SSH_PASSWORD = var.SSH_PASSWORD
+#   prometheus_servers = var.prometheus_servers
+#   zone_id       = data.aws_route53_zone.main.zone_id
+#
+# }
