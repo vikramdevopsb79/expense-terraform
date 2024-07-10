0b37bd3e1385003806addc1f41659eb136b5062d
sai <kuppalasaivikram@gmail.com>
2024-07-10 11:22:52 +0530

    Expense terraform project using vpc

--- variables.tf
+++ variables.tf
@@ -1,8 +1,9 @@
 # variable "components" {}
 
-# variable "vault_token" {}
+#
 # variable "SSH_PASSWORD" {}
 # variable "prometheus_servers" {}
 # variable "ami" {}
 variable "env" {}
 variable "vpc" {}
+variable "vault_token" {}
