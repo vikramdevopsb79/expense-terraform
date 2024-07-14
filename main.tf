37f71aa573780ca7461489279bd92e51acc86f3b
sai <kuppalasaivikram@gmail.com>
2024-07-14 15:15:20 +0530

    output

--- main.tf
+++ main.tf
@@ -75,9 +75,7 @@ module "eks" {
   eks_version = each.value["eks_version"]
   node_groups = each.value["node_groups"]
 
-  subnet_ids      = module.vpc.db_subnet_ids
-  eks_subnet_cidr = module.vpc.eks_subnet_cidr
-  vpc_id          = module.vpc.vpc_id
+  subnet_ids = module.vpc["main"].eks_subnet_ids
 
   tags = var.tags
   env  = var.env
--- variables.tf
+++ variables.tf
@@ -1,14 +1,16 @@
 # variable "components" {}
 
 #
-variable "SSH_PASSWORD" {}
+
 # variable "prometheus_servers" {}
 # variable "ami" {}
-variable "env" {}
-variable "vpc" {}
-variable "vault_token" {}
+
 # variable "lb_subnet_cidr" {}
 # variable "eks_subnet_cidr" {}
 # variable "db_subnet_cidr" {}
+variable "SSH_PASSWORD" {}
 variable "tags" {}
 variable "eks" {}
+variable "env" {}
+variable "vpc" {}
+variable "vault_token" {}
