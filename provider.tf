7f63b62aaef53d6309fcc0949dffe56b945da903
sai <kuppalasaivikram@gmail.com>
2024-07-16 15:38:23 +0530

    vault provider is added

--- provider.tf
+++ provider.tf
@@ -1,3 +1,10 @@
 terraform {
   backend "s3" {}
 }
+provider "http" {}
+
+provider "vault" {
+  address         = "https://vault-internal.vikramdevops.tech:8200"
+  token           = var.vault_token
+  skip_tls_verify = true
+}
