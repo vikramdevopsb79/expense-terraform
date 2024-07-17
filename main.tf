51987e432ee9f049bc024ce5a604e6f38e5d7b20
sai <kuppalasaivikram@gmail.com>
2024-07-17 15:54:57 +0530

    uncommet

--- main.tf
+++ main.tf
@@ -78,16 +78,16 @@ module "eks" {
   env  = var.env
 }
 
-module "rds" {
-  source = "git::https://github.com/vikramdevopsb79/tf-module-rds.git"
-
-  for_each        = var.rds
-  engine          = each.value["engine"]
-  engine_version  = each.value["engine_version"]
-  env             = var.env
-  family          = each.value["family"]
-  instance_class  = each.value["instance_class"]
-  subnet_ids      = module.vpc["main"].db_subnet_ids
-  eks_subnet_cidr = module.vpc["main"].eks_subnet_cidr
-  vpc_id          = module.vpc["main"].vpc_id
-}
+# module "rds" {
+#   source = "git::https://github.com/vikramdevopsb79/tf-module-rds.git"
+#
+#   for_each        = var.rds
+#   engine          = each.value["engine"]
+#   engine_version  = each.value["engine_version"]
+#   env             = var.env
+#   family          = each.value["family"]
+#   instance_class  = each.value["instance_class"]
+#   subnet_ids      = module.vpc["main"].db_subnet_ids
+#   eks_subnet_cidr = module.vpc["main"].eks_subnet_cidr
+#   vpc_id          = module.vpc["main"].vpc_id
+# }
